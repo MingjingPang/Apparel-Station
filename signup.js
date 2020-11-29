326022041a56c300d112e3b4bc49957297fe52fb
MingjingPang <54654612+MingjingPang@users.noreply.github.com>
2020-11-29 15:18:11 -0800

    firebase

--- signup.js
+++ signup.js
@@ -1,36 +1,17 @@
-function initApp() {
-    document.getElementById('signup').addEventListener('click', handleSignUp, false);
-  }
-  
-  function handleSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-  
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // [START_EXCLUDE]
-      if (errorCode == 'auth/weak-password') {
-        alert('The password is too weak.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);
-      // [END_EXCLUDE]
-    });
-    // [END createwithemail]
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-      if(firebaseUser) {
-        console.log(firebaseUser);
-        window.location = 'login.html';
-      } else {
-        console.log('not logged in');
-      }
-    });
-    //window.location.href = 'home.html';
-  }
-  
-  window.onload = function() {
-    initApp();
-  }
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = 
+  {
+    apiKey: "AIzaSyAabP0_wNf6rRz4q_YFjwPps4MOA0jZpiM",
+    authDomain: "apparel-station.firebaseapp.com",
+    databaseURL: "https://apparel-station.firebaseio.com",
+    projectId: "apparel-station",
+    storageBucket: "apparel-station.appspot.com",
+    messagingSenderId: "342533671958",
+    appId: "1:342533671958:web:57f077903c31308e11f937",
+    measurementId: "G-WBY1WDERHP"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+    const auth = firebase.auth();
