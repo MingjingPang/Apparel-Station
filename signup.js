16988dc8dcecae3ff3b923d6878bc4e4538a3cac
MingjingPang <54654612+MingjingPang@users.noreply.github.com>
2020-11-29 15:33:25 -0800

    firebase

--- signup.js
+++ signup.js
@@ -15,3 +15,14 @@
   firebase.initializeApp(firebaseConfig);
 
     const auth = firebase.auth();
+
+
+    function signUp(){
+        var email =  document.getElementById("email");
+        var password =  document.getElementById("password");
+
+        const promise = auth.createUserWithEmailAndPassword(email.value.password.value); 
+        promise.catch(e => alert(e.message));
+
+        alert("Signed Up")
+    }
